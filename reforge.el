702b66b0cdb59ad727961c3f11708dcb757fdd57
Naoya Yamashita <conao3@gmail.com>
2020-12-31 04:25:39 +0900

    require promise to async connection

--- reforge.el
+++ reforge.el
@@ -5,7 +5,7 @@
 ;; Author: Naoya Yamashita <conao3@gmail.com>
 ;; Version: 0.0.1
 ;; Keywords: convenience
-;; Package-Requires: ((emacs "26.1") (magit "2.90"))
+;; Package-Requires: ((emacs "26.1") (magit "2.90") (promise "1.1"))
 ;; URL: https://github.com/conao3/reforge.el
 
 ;; This program is free software: you can redistribute it and/or modify
