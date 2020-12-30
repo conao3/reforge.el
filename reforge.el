bd642664ca056388c9489a986d87216ec78582d3
Naoya Yamashita <conao3@gmail.com>
2020-12-31 02:35:30 +0900

    generate project

---
+++ reforge.el
@@ -0,0 +1,38 @@
+;;; reforge.el --- Yet another forge; collaborate on GitHub using Magit  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; Version: 0.0.1
+;; Keywords: convenience
+;; Package-Requires: ((emacs "26.1"))
+;; URL: https://github.com/conao3/reforge.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Yet another forge; collaborate on GitHub using Magit.
+
+
+;;; Code:
+
+(defgroup reforge nil
+  "Yet another forge; collaborate on GitHub using Magit."
+  :group 'convenience
+  :link '(url-link :tag "Github" "https://github.com/conao3/reforge.el"))
+
+(provide 'reforge)
+
+;;; reforge.el ends here
